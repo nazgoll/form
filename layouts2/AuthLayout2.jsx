2554bb12863ee8124bc86f10d3b09304e02acff1
nazgol <nazgool.momeni1@gmail.com>
2023-07-31 21:09:47 +0330

    dark

--- layouts2/AuthLayout2.jsx
+++ layouts2/AuthLayout2.jsx
@@ -1,10 +1,10 @@
-const AuthLayout = (props) => {
+const AuthLayout2 = (props) => {
     return(
-        <div className={"w-screen h-screen items-center justify-center flex flex-col bg-gradient-to-r from-blue-100 to-color-4"}>
-            <div  className={"bg-white p-16 pb-20 shadow-inner shadow-black " +
+        <div className={"w-screen h-screen items-center justify-center flex flex-col bg-color-1"}>
+            <div  className={"bg-white p-16 pb-20 shadow-inner shadow-black bg-gradient-to-r from-blue-100 to-color-4" +
                 " shadow-lg rounded-xl items-center justify-center flex  flex-col shadow-black" +
                 "w-3/12 h-3/5 md:h-4/12 md:w-3/12 "}>
-                <div class={""}></div>
+                <div className={"rounded-full bg-color-1 w-32 h-32"}></div>
                 <header className=" text-6xl font-extrabold text-color-6  mb-10 text-shadow-white ">
                     {props.headerText}
                 </header>
@@ -13,4 +13,4 @@ const AuthLayout = (props) => {
         </div>
     )
 }
-export default AuthLayout;
+export default AuthLayout2;
---
+++ pages/login2/index.jsx
@@ -0,0 +1,58 @@
+import AuthLayout2 from "@/layouts2/AuthLayout2";
+import {useState} from "react";
+const LoginPage = () => {
+    const [username, setUsername] =
+        useState("");
+    const [password, setPassword] = useState("");
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    }
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+    return(
+        <AuthLayout2 headerText={"Login"}>
+            <div className={"flex flex-col gap-8"}>
+                <div>
+                    <label className={"text-white font-bold  w-full"}>
+                        User name
+                    </label>
+                    <input
+                        className={"rounded-lg mx-3 w-full p-2 bg-white shadow-md shadow-black hover:bg-yellow-400"}
+                        type={"text"}
+                        value={username}
+                        onChange={handleUsernameChange}
+                        placeholder="email/name"
+                    />
+                </div>
+                <div>
+                    <label className={"text-white font-bold  w-full"}>
+                        Password
+                    </label>
+                    <input
+                        className={"rounded-lg mx-3 w-full p-2 bg-white shadow-md shadow-black hover:bg-yellow-400"}
+                        type={"password"}
+                        value={password}
+                        onChange={handlePassword}
+                        placeholder="password"
+                    />
+                </div>
+
+                <div className={"flex flex-row items-center"}>
+                    <a href={"/signup"}
+                       className={"py-3 px-7 ml-9 mr-3 w-2/6 mt-3 bg-yellow-400  rounded-xl text-2xl font-bold hover:bg-blue-100 text-black" +
+                           ""}>
+                        Login
+                    </a>
+                    <p className={"text-2xl ml-2 w-1/12 pr-2 text-white"}> / </p>
+                    <a className={"border-r-amber-400 py-3 px-7 w-2/5 mt-3 mr-3 rounded-xl text-2xl font-bold text-white hover:text-blue-100"}
+                       href="">
+                        Sign up
+                    </a>
+                </div>
+            </div>
+        </AuthLayout2>
+
+    )
+}
+export default LoginPage;
